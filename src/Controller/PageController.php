9e520ce2aa36d4f22f8088cd2019c659bacffacc
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2023-02-05 16:28:05 -0600

    Se agregaron los componentes para el request y el response

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,12 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class PageController
 {
     #[Route('/')]
-    public function home()
+    public function home( Request $request ) : Response
     {
-        return "Hello wordl!";
+        $search = $request->get('search');
+        return new Response("Hello wordl!", $search);
     }
 }
